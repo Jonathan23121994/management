6dc69337ddc1135f8e9f69fd70144e1fec8f6cf1
Jonathan Alexander <jonathanalex94@hotmail.com>
2022-01-30 15:02:06 -0300

    adicionei pagina

--- site.py
+++ site.py
@@ -11,6 +11,13 @@ app = Flask(__name__) # padrão flask
 def homepage():# função significa oque você quer exibir naquela pagina
     return render_template('pagina1.html')# é preciso retornar um valor da função para a página 'pagina1.html' chama o html 
 
+@app.route('/maner') #caminho depois do meu domínio, a barrinha só significa primeira página
+
+
+def maner():# função significa oque você quer exibir naquela pagina
+    return render_template('pagina2.html')
+
+
 if __name__=='__main__':#esse if é p não da problema no servidor
 
     app .run(debug=True)# debug=True é pro sit atualizar sozinho 
